6f51e8467a89132be37e7e00bfff539c81a1b4c2
Viraj Tank <virajtank98@gmail.com>
2023-02-16 11:37:10 +0530

    using savedInstanceState in Activity

--- app/src/main/java/com/novuspax/androidvisionboard/MainActivity.kt
+++ app/src/main/java/com/novuspax/androidvisionboard/MainActivity.kt
@@ -1,31 +1,59 @@
 package com.novuspax.androidvisionboard
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.Button
 import android.widget.RelativeLayout
-import androidx.core.view.ViewCompat.animate
 import com.novuspax.androidvisionboard.databinding.ActivityMainBinding
+import com.novuspax.androidvisionboard.utils.Constants.COUNTER_STATE_KEY
+import com.novuspax.androidvisionboard.utils.Constants.TEXT_SCALE_KEY
 
 class MainActivity : AppCompatActivity() {
 
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
-    var rotVal = 0.9f
-    private val TAG = "MainActivity"
+    var textScale = 0.9f
+    var counter = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        savedInstanceState?.let {
+            counter = it.getInt(COUNTER_STATE_KEY)
+            textScale = it.getFloat(TEXT_SCALE_KEY)
+        }
         setCustomContentView()
+        binding.tv.apply {
+            text = "$counter"
+            scaleX = textScale
+            scaleY = textScale
+        }
 
         binding.btnClick.setOnClickListener {
-            rotVal += 1
-            binding.tv.animate().scaleX(rotVal)
+            textScale += 0.5f
+            counter += 1
+            binding.tv.text = "$counter"
+            binding.tv.animate().scaleX(textScale)
+            binding.tv.animate().scaleY(textScale)
+
         }
+    }
+
+    // when configuration changes happen we store data to bundle
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(COUNTER_STATE_KEY, counter)
+        outState.putFloat(TEXT_SCALE_KEY, textScale)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        // super needs to call first for restore
+        super.onRestoreInstanceState(savedInstanceState)
+        binding.tv.text = "${savedInstanceState.getInt(COUNTER_STATE_KEY)}"
+        textScale = savedInstanceState.getFloat(TEXT_SCALE_KEY)
     }
 
     private fun setCustomContentView() {
@@ -42,6 +70,6 @@ class MainActivity : AppCompatActivity() {
         myLayout.addView(button)
 
 //        setContentView(myLayout,myLayoutParams)
-          setContentView(binding.root)
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/novuspax/androidvisionboard/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.novuspax.androidvisionboard.utils
+
+object Constants {
+    const val COUNTER_STATE_KEY = "counter"
+    const val TEXT_SCALE_KEY = "textScale"
+}
